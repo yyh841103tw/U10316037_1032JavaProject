d18184e4dd3a017e02659157803bd14b568ecc20
yyh841103tw <yyh841103tw@gmail.com>
2015-06-01 16:18:48 +0800

    Create OOXX.java

---
+++ OOXX.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class OOXX extends JFrame{
+
+public static void main(String[] args){
+			
+		
+		
+		OOXX frame = new OOXX();
+
+		
+		frame.setTitle("OOXX");
+		frame.setVisible(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		}
+		
+	}
